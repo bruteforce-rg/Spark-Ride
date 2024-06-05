5d4129189ec286e77f0eb3a612e1f61f6ddb1564
Rakesh Garai <70056512+bruteforce-rg@users.noreply.github.com>
2024-06-05 20:11:39 +0530

    Add files via upload

---
+++ spark_ride.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2024 at 04:39 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spark_ride`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_user`
+--
+
+CREATE TABLE `admin_user` (
+  `id` int(11) NOT NULL,
+  `Username` varchar(25) NOT NULL,
+  `Password` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_user`
+--
+
+INSERT INTO `admin_user` (`id`, `Username`, `Password`) VALUES
+(1, 'Rakesh', 'password'),
+(2, 'Amrita', 'iamadmin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`id`, `name`, `image`) VALUES
+(41, 'Bajaj', '../Brand Logo/Bajaj.jpg'),
+(42, 'Yamaha', '../Brand Logo/Yamaha.jpg'),
+(44, 'Royal Enfield', '../Brand Logo/Royal Enfield.jpg'),
+(45, 'TVS', '../Brand Logo/TVS.jpg'),
+(46, 'KTM', '../Brand Logo/KTM.jpg'),
+(47, 'JAWA', '../Brand Logo/JAWA.jpg'),
+(49, 'Aprilia', '../Brand Logo/Aprilia.jpg'),
+(50, 'BMW', '../Brand Logo/BMW.jpg'),
+(54, 'Hero', '../Brand Logo/Hero.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` varchar(20) NOT NULL,
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `dl_no` varchar(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `dob` date NOT NULL,
+  `phone_no` bigint(100) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `pincode` int(10) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `reg_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `otp` int(11) NOT NULL,
+  `active` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `fname`, `lname`, `dl_no`, `email`, `password`, `dob`, `phone_no`, `city`, `state`, `pincode`, `address`, `reg_time`, `otp`, `active`) VALUES
+('SPR148514', 'Amrita', 'Ghosh', 'Wb157vgh', 'amrita261103@gmail.com', '00/CNx+q8DXd+ptxtOYsAw==', '0000-00-00', 9339707050, '', '', 0, '', '0000-00-00 00:00:00', 0, '0'),
+('SPR901473', 'rakesh', 'garai', '1', 'rakeshgarai23@gmail.com', 'kQx4JEBCmQIH/+WjcKpnNw==', '0000-00-00', 1, '', '', 0, '', '2024-06-02 19:20:00', 568072, '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone_no` (`phone_no`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
